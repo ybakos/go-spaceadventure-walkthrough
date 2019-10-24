9f9482ddb7c7675919e9f6e133aef4264bf23f20
Yong Joseph Bakos <ybakos@humanoriented.com>
2019-10-23 21:36:13 -0700

    Initial commit. Start with some functions and smells.

---
+++ README.md
@@ -0,0 +1,26 @@
+# Go Space Adventure (Walkthrough)
+
+A simple exploration of the solar system (or, really, an exploration of the Go language).
+
+## Example Text Input/Output (The "Spec")
+
+```
+Welcome to the Solar System!
+There are 8 planets to explore.
+What is your name?
+Jane
+Nice to meet you, Jane. My name is Eliza, I'm an old friend of Siri.
+Let's go on an adventure!
+Shall I randomly choose a planet for you to visit? (Y or N)
+N
+Name the planet you would like to visit.
+Neptune
+Traveling to Neptune...
+Arrived at Neptune. A very cold planet, furthest from the sun.
+```
+
+## Ideas
+
+TODO
+
+&copy; 2019 Yong Joseph Bakos. All rights reserved.
---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import "fmt"
+
+func main() {
+	printWelcome()
+	name := getName()
+	printGreeting(name)
+	fmt.Println("Let's go on an adventure!")
+	travel()
+}
+
+func printWelcome() {
+	fmt.Println("Welcome to the Solar System!")
+	fmt.Println("There are 8 planets to explore.")
+}
+
+func getName() string{
+	var name string
+	fmt.Println("What is your name?")
+    fmt.Scan(&name)
+    return name
+}
+
+func printGreeting(name string) {
+	fmt.Printf("Nice to meet you, %s. My name is Eliza, I'm an old friend of Siri.\n", name)
+}
+
+func travel() {
+	var choice string
+	for choice != "Y" && choice != "N" {
+		choice = getTravelChoice()
+		if choice == "Y" {
+			travelToRandomPlanet()
+		} else if choice == "N" {
+			planetName := getPlanetName()
+			travelToPlanet(planetName)
+		} else {
+			fmt.Println("Sorry, I didn't get that.")
+		}
+	}
+}
+
+func getTravelChoice() string {
+	var choice string
+	fmt.Println("Shall I randomly choose a planet for you to visit? (Y or N)")
+	fmt.Scan(&choice)
+	return choice
+}
+
+func travelToRandomPlanet() {
+	fmt.Println("Traveling to Jupiter...")
+	fmt.Println("Arrived at Jupiter. The large red spot appears ominous.")
+}
+
+func getPlanetName() string {
+	var name string
+	fmt.Println("Name the planet you would like to visit.")
+	fmt.Scan(&name)
+	return name
+}
+
+func travelToPlanet(planetName string) {
+	fmt.Printf("Traveling to %s...\n", planetName)
+	fmt.Println("Arrived at Neptune. A very cold planet, furthest from the sun.")
+}
