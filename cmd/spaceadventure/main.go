ca6b4a40f5a3040f265dcd72dc779e4232f266e0
Yong Joseph Bakos <ybakos@humanoriented.com>
2019-11-08 11:32:50 -0800

    Extract functions into external file.

--- cmd/spaceadventure/main.go
+++ cmd/spaceadventure/main.go
@@ -1,11 +1,7 @@
 package main
 
-import "fmt"
 import "github.com/ybakos/spaceadventure_walkthrough/internal/spaceadventure"
 
 func main() {
-	spaceadventure.PrintWelcome()
-	spaceadventure.PrintGreeting(spaceadventure.ResponseToPrompt("What is your name?"))
-	fmt.Println("Let's go on an adventure!")
-	spaceadventure.Travel()
+	spaceadventure.Start()
 }
--- internal/spaceadventure/spaceadventure.go
+++ internal/spaceadventure/spaceadventure.go
@@ -2,42 +2,49 @@ package spaceadventure
 
 import "fmt"
 
-func PrintWelcome() {
+func Start() {
+	printWelcome()
+	printGreeting(responseToPrompt("What is your name?"))
+	fmt.Println("Let's go on an adventure!")
+	travel()
+}
+
+func printWelcome() {
 	fmt.Println("Welcome to the Solar System!")
 	fmt.Println("There are 8 planets to explore.")
 }
 
-func ResponseToPrompt(prompt string) string {
+func responseToPrompt(prompt string) string {
 	var response string
 	fmt.Println(prompt)
 	fmt.Scan(&response)
 	return response
 }
 
-func PrintGreeting(name string) {
+func printGreeting(name string) {
 	fmt.Printf("Nice to meet you, %s. My name is Eliza, I'm an old friend of Siri.\n", name)
 }
 
-func Travel() {
+func travel() {
 	var choice string
 	for choice != "Y" && choice != "N" {
-		choice = ResponseToPrompt("Shall I randomly choose a planet for you to visit? (Y or N)")
+		choice = responseToPrompt("Shall I randomly choose a planet for you to visit? (Y or N)")
 		if choice == "Y" {
-			TravelToRandomPlanet()
+			travelToRandomPlanet()
 		} else if choice == "N" {
-			TravelToPlanet(ResponseToPrompt("Name the planet you would like to visit."))
+			travelToPlanet(responseToPrompt("Name the planet you would like to visit."))
 		} else {
 			fmt.Println("Sorry, I didn't get that.")
 		}
 	}
 }
 
-func TravelToRandomPlanet() {
+func travelToRandomPlanet() {
 	fmt.Println("Traveling to Jupiter...")
 	fmt.Println("Arrived at Jupiter. The large red spot appears ominous.")
 }
 
-func TravelToPlanet(planetName string) {
+func travelToPlanet(planetName string) {
 	fmt.Printf("Traveling to %s...\n", planetName)
 	fmt.Println("Arrived at Neptune. A very cold planet, furthest from the sun.")
 }
